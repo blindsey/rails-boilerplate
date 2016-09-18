2c7e39b7c8b13201d477626c0d9f853538ae71fc
Ben Lindsey <ben.lindsey@gmail.com>
2016-09-18 16:56:38 -0700

    Fixes for deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Fillory
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
     config.colorize_logging = false
   end
 end
